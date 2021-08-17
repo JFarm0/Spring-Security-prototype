205339821f6fc744a04f26a986ba3493c25b7f0b
JFarm0 <79594115+JFarm0@users.noreply.github.com>
2021-08-17 16:23:56 -0500

    Initial Commit

---
+++ spring-boot-security-proof/src/main/java/com/example/springbootsecurityproof/SpringBootSecurityProofApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecurityproof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityProofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityProofApplication.class, args);
+	}
+
+}
---
+++ spring-boot-security-proof/src/test/java/com/example/springbootsecurityproof/SpringBootSecurityProofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecurityproof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityProofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
